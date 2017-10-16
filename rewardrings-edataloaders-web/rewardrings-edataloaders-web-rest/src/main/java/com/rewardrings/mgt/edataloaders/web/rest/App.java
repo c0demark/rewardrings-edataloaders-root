8c41ad1b8b4e3b6db33667ddc1f4ab990a73a8b7
c0demark <kumarvijay336@gmail.com>
2017-10-17 00:57:18 +0530

    corrected the package structure

---
+++ rewardrings-edataloaders-web/rewardrings-edataloaders-web-rest/src/main/java/com/rewardrings/mgt/edataloaders/web/rest/App.java
@@ -0,0 +1,15 @@
+package com.rewardrings.mgt.edataloaders.web.rest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Vijay Kumar
+ *
+ */
+@SpringBootApplication
+public class App {
+    public static void main( String[] args ) {
+        SpringApplication.run(App.class, args);
+    }
+}
